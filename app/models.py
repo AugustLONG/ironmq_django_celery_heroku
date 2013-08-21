402629186f6d0b5e465ab0ddd6e1027bbe40d54f
Brad Van Vugt <bvanvugt@gmail.com>
2013-08-20 23:07:39 -0700

    Add test case.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Thing(models.Model):
+class Task(models.Model):
     name = models.CharField(max_length=256)
     complete = models.BooleanField(default=False)
 
--- app/tasks.py
+++ app/tasks.py
@@ -10,9 +10,35 @@ from models import Task
 logger = logging.getLogger(__name__)
 
 
+# Test Runner
+
+def run_test(num_masters=4, num_tasks=100000):
+    tasks_per_master = num_tasks / num_masters
+    for m in range(num_masters):
+        queue_task_creation(str(m), tasks_per_master)
+
+
+# Master Task Queue-er
+
+def queue_task_creation(master_name, num_tasks):
+    handle_task_creation(master_name, num_tasks)
+
+@task(queue=settings.QUEUES.MASTER)
+def handle_task_creation(master_name, num_tasks):
+    for x in range(num_tasks):
+        # Create task
+        task_name = '%s-%s' % (master_name, x)
+        task = Task(name=task_name)
+        task.save()
+
+        # Queue it for "completion"
+        queue_task(task)
+
+
+# Task Worker
+
 def queue_task(task):
     handle_task.delay(task.id)
-
 
 @task(queue=settings.QUEUES.WORKER)
 def handle_task(task_id):
